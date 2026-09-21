5cfb1d6aaa0a31823e24c0ce423810f4ca39e429
Annie Chang <chun@Annie-Chang-2.local>
2026-09-22 06:52:07 +0800

    Wave 2 done

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,5 @@
+from .item import Item
+
 class Vendor:
     def __init__(self, inventory = []):
         self.inventory = inventory
@@ -8,10 +10,15 @@ class Vendor:
 
     def remove(self, matching_item):
         for i in range(len(self.inventory)):
-            print(self.inventory[i])
-            print(matching_item)
             if self.inventory[i] == matching_item:
                 self.inventory.pop(i)
                 return matching_item
         return None
+
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
+    
             
