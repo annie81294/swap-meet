61684d15fee0c72980997dd0347b5de70c1bc1f5
Annie Chang <chun@Annie-Chang-2.local>
2026-09-22 05:36:47 +0800

    Wave 1 done

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, matching_item):
+        for i in range(len(self.inventory)):
+            print(self.inventory[i])
+            print(matching_item)
+            if self.inventory[i] == matching_item:
+                self.inventory.pop(i)
+                return matching_item
+        return None
+            
