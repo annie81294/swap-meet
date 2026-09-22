ee247d1c6777206046b7d48d82d26722d96250ce
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2026-09-22 13:55:44 -0700

    Wave 3 complete

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -20,5 +20,15 @@ class Vendor:
             if item.id == id:
                 return item
         return None
-    
-            
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if not self.get_by_id(my_item.id) or not other_vendor.get_by_id(their_item.id):
+            return False
+        
+        self.remove(my_item)
+        other_vendor.add(my_item)
+        self.add(their_item)
+        other_vendor.remove(their_item)
+        print('after', other_vendor.inventory)
+
+        return True
